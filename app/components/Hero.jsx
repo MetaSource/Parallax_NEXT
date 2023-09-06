1997583927b402c0233b320808e799069e2e0fc8
MetaSource <jsal91@gmail.com>
2023-09-05 21:11:33 -0700

    add height to overlay

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Hero = ({businessName, city, buttonLabel}) => {
   return (
-    <div className='flex items-center justify-center h-screen bg-device sm:bg-device-sm bg-center bg-fixed bg-cover'>
+    <div className='flex items-center justify-center h-screen bg-device sm:bg-device-sm bg-center bg-scroll sm:bg-fixed bg-cover'>
         {/*overlay*/}
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
+        <div className='absolute h-screen top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
 
         <div className='p-5 text-white z-[2] mt-[-10rem]'>
             <h2 className='text-5xl cinzel'>{businessName}</h2>
